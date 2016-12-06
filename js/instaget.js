2557cb4bd83b3fcc55681bf7a693f008f14cd30b
alexander.polyakov <alexander.polyakov@breffi.ru>
2016-12-06 19:05:59 +0300

    added gitignore

--- js/instaget.js
+++ js/instaget.js
@@ -1,23 +1,34 @@
 /**
  * Created by propo on 06.12.2016.
  */
-$(function () {
-    var token = '691623.1419b97.479e4603aff24de596b1bf18891729f3',
-        userid = 691623,
-        num_photos = 20;
-    $.ajax({
-        url: 'https://api.instagram.com/v1/users/' + userid + '/media/recent',
-        dataType: 'jsonp',
-        type: 'GET',
-        data: {access_token: token, count: num_photos},
-        success: function (data) {
-            console.log(data);
-            for (x in data.data) {
-                $('#instaView').append('<p><img src="' + data.data[x].images.low_resolution.url + '" data-id="' + data.data[x].id + '"></p>');
-            }
-        },
-        error: function (data) {
-            console.log(data);
-        }
-    });
-});
+var token = '691623.1419b97.479e4603aff24de596b1bf18891729f3',
+    userid = 691623,
+    num_photos = 20;
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://api.instagram.com/v1/users/' + userid + '/media/recent?access_token = 691623.1419b97.479e4603aff24de596b1bf18891729f3&count = 20&callback=getJSONP', true);
+xhr.onload = function () {
+   // var data = JSON.parse(xhr.response);
+    alert(data);
+};
+
+
+// $(function () {
+//     var token = '691623.1419b97.479e4603aff24de596b1bf18891729f3',
+//         userid = 691623,
+//         num_photos = 20;
+//     $.ajax({
+//         url: 'https://api.instagram.com/v1/users/' + userid + '/media/recent',
+//         dataType: 'jsonp',
+//         type: 'GET',
+//         data: {access_token: token, count: num_photos},
+//         success: function (data) {
+//             console.log(data);
+//             for (x in data.data) {
+//                 $('#instaView').append('<p><img id="' + data.data[x].id + '" src="' + data.data[x].images.low_resolution.url + '"></p>');
+//             }
+//         },
+//         error: function (data) {
+//             console.log(data);
+//         }
+//     });
+// });
