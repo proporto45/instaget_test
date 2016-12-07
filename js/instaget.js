dc20fc815ad89befc1363ffbda2b0dfb2ec50263
alexander.polyakov <alexander.polyakov@breffi.ru>
2016-12-07 19:05:08 +0300

    commi

--- js/instaget.js
+++ js/instaget.js
@@ -1,17 +1,29 @@
 /**
  * Created by propo on 06.12.2016.
  */
-var token = '691623.1419b97.479e4603aff24de596b1bf18891729f3',
-    userid = 691623,
-    num_photos = 20;
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://api.instagram.com/v1/users/' + userid + '/media/recent?access_token = 691623.1419b97.479e4603aff24de596b1bf18891729f3&count = 20&callback=getJSONP', true);
-xhr.onload = function () {
-   // var data = JSON.parse(xhr.response);
-    alert(data);
-};
+document.addEventListener('DOMContentLoaded', function () {
 
+    var token = '691623.1419b97.479e4603aff24de596b1bf18891729f3',
+        userid = 691623,
+        num_photos = 20;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.instagram.com/v1/users/' + userid + '/media/recent/?access_token=' + token, true);
+    xhr.send();
+    xhr.onload = function () {
+        // var data = JSON.parse(xhr.response);
+        console.log(xhr);
+// xhr.onreadystatechange = function () {
+        if (xhr.status != 200) {
+            // обработать ошибку
+            alert(xhr.status); // пример вывода: 404: Not Found
+            // alert( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
+        } else {
+            // вывести результат
+            alert(xhr.response); // responseText -- текст ответа.
+        }
+    }
 
+});
 // $(function () {
 //     var token = '691623.1419b97.479e4603aff24de596b1bf18891729f3',
 //         userid = 691623,
